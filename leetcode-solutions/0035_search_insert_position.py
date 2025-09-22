3efa6eab308b9c1aeacde1584eb959308bac53ed
kethashishir <77924243+kethashishir@users.noreply.github.com>
2025-09-22 07:56:15 -0700

    Day 9: Added LeetCode #35 Search Insert Position

---
+++ leetcode-solutions/0035_search_insert_position.py
@@ -0,0 +1,40 @@
+# LeetCode 35: Search Insert Position
+# Link: https://leetcode.com/problems/search-insert-position/
+# Approach: Binary search to find target or correct insert index
+# Time Complexity: O(log n)
+# Space Complexity: O(1)
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        low = 0
+        high = len(nums) - 1
+
+        if low == high:
+            if nums[low] == target:
+                return low
+            elif nums[low] < target:
+                return low + 1
+            else:
+                return low
+
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return low
+
+# Example run
+print(Solution().searchInsert([1,3,5,6], 5))  # Output: 2
+print(Solution().searchInsert([1,3,5,6], 2))  # Output: 1
+print(Solution().searchInsert([1,3,5,6], 7))  # Output: 4
+print(Solution().searchInsert([1,3,5,6], 0))  # Output: 0
